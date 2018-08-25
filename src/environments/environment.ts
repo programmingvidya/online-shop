6c0cd7e31c451d8a9d677a2384cd67ae5182ce35
programmingvidya <programmingvidya@gmail.com>
2018-08-25 16:36:25 +0100

    Firebase environment settings changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAwxo8Bt7ZeYfFLI5WrakbNGtJsskBv3wA",
-    authDomain: "oshop-f3445.firebaseapp.com",
-    databaseURL: "https://oshop-f3445.firebaseio.com",
-    projectId: "oshop-f3445",
-    storageBucket: "oshop-f3445.appspot.com",
-    messagingSenderId: "223112932947"    
+    apiKey: "AIzaSyCSDZG-4xSSBRHKBUufCzvz5DVC-SVu2so",
+    authDomain: "fir-demo-bf2d0.firebaseapp.com",
+    databaseURL: "https://fir-demo-bf2d0.firebaseio.com",
+    projectId: "fir-demo-bf2d0",
+    storageBucket: "fir-demo-bf2d0.appspot.com",
+    messagingSenderId: "794599561272"
   }
 };
