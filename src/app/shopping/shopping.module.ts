a3362f286923e94afe387f3c689ec9f57e586ff8
programmingvidya <programmingvidya@gmail.com>
2018-08-25 17:46:49 +0100

    Authentication temporarily disabled for checkout page

--- src/app/shopping/shopping.module.ts
+++ src/app/shopping/shopping.module.ts
@@ -1,28 +1,40 @@
-import { SharedModule } from './../shared/shared.module';
-import { AuthGuard } from 'shared/services/auth-guard.service';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { SharedModule } from "./../shared/shared.module";
+import { AuthGuard } from "shared/services/auth-guard.service";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
 
-import { CheckOutComponent } from './components/check-out/check-out.component';
-import { MyOrdersComponent } from './components/my-orders/my-orders.component';
-import { OrderSuccessComponent } from './components/order-success/order-success.component';
-import { ProductFilterComponent } from './components/products/product-filter/product-filter.component';
-import { ProductsComponent } from './components/products/products.component';
-import { ShippingFormComponent } from './components/shipping-form/shipping-form.component';
-import { ShoppingCartSummaryComponent } from './components/shopping-cart-summary/shopping-cart-summary.component';
-import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from "./components/check-out/check-out.component";
+import { MyOrdersComponent } from "./components/my-orders/my-orders.component";
+import { OrderSuccessComponent } from "./components/order-success/order-success.component";
+import { ProductFilterComponent } from "./components/products/product-filter/product-filter.component";
+import { ProductsComponent } from "./components/products/products.component";
+import { ShippingFormComponent } from "./components/shipping-form/shipping-form.component";
+import { ShoppingCartSummaryComponent } from "./components/shopping-cart-summary/shopping-cart-summary.component";
+import { ShoppingCartComponent } from "./components/shopping-cart/shopping-cart.component";
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: 'products', component: ProductsComponent },
-      { path: 'shopping-cart', component: ShoppingCartComponent },
-      { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard] },
-      { path: 'order-success/:id', component: OrderSuccessComponent, canActivate: [AuthGuard] },
-      { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
+      { path: "products", component: ProductsComponent },
+      { path: "shopping-cart", component: ShoppingCartComponent },
+      {
+        path: "check-out",
+        component: CheckOutComponent
+        //, canActivate: [AuthGuard]
+      },
+      {
+        path: "order-success/:id",
+        component: OrderSuccessComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: "my/orders",
+        component: MyOrdersComponent,
+        canActivate: [AuthGuard]
+      }
     ])
   ],
   declarations: [
@@ -36,4 +48,4 @@ import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.
     ShippingFormComponent
   ]
 })
-export class ShoppingModule { }
+export class ShoppingModule {}
